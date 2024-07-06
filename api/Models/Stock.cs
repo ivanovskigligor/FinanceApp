3f1e64df26d1b4b6d23ec561e700721c81bce9cf
Gligor Ivanovski <ivanovskigligor@gmail.com>
2024-07-06 02:27:00 +0200

    initial

---
+++ api/Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public int Id{ get; set; }
+
+        public string Symbol{ get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(18.2)")];
+        
+        public decimal Purchase { get; set; }
+
+        public decimal LastDiv{ get; set; }
+    }
+}
+
---
+++ frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./Card.css";
+import { CompanySearch } from '../../company';
+type Props = {
+    id: string;
+    searchResult: CompanySearch;
+
+}
+
+const Card : React.FC<Props> = ({id, searchResult}: Props): JSX.Element => {
+    return (
+        <div className='card'>
+            <img 
+            alt='Image'
+            />
+            <div className='details'>
+                <h2>{searchResult.name} ({searchResult.symbol})</h2>
+                <p>${searchResult.currency}</p>
+            </div>
+            <p className='information'>
+                {searchResult.exchangeShortName} - {searchResult.stockExchange}
+            </p>
+        </div>
+    )
+}
+
+export default Card
